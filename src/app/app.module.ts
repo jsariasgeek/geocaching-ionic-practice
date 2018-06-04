34d17a5805fa5a0354a66620387873f75ab1596c
Johán Steven Arias <jsarias1993@gmail.com>
2018-06-03 22:12:53 -0500

    Lets go to getting started with the firebase connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,52 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {LugarPage} from "../pages/lugar/lugar";
+import {PerfilPage} from "../pages/perfil/perfil";
+import {TabsPage} from "../pages/tabs/tabs";
+import {TerceraPage} from "../pages/tercera/tercera";
+import {AngularFireModule} from "angularfire2";
+import {AngularFireAuthModule} from "angularfire2/auth";
+import {AngularFireDatabaseModule, AngularFireDatabase} from "angularfire2/database";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDYyfVxWxddSQrkH0AbpqNgujW7PkqouUE",
+  authDomain: "ionicbasicplatzi-28126.firebaseapp.com",
+  databaseURL: "https://ionicbasicplatzi-28126.firebaseio.com",
+  projectId: "ionicbasicplatzi-28126",
+  storageBucket: "ionicbasicplatzi-28126.appspot.com",
+  messagingSenderId: "679215339785"
+}
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LugarPage,
+    PerfilPage,
+    TabsPage,
+    TerceraPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LugarPage,
+    PerfilPage,
+    TabsPage,
+    TerceraPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireDatabase,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
